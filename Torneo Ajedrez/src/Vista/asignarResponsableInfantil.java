728656c4410c344cfcbcaac2091730903fdc8a9c
Ayoub El Moussaoui <aelmousa@alumni.uv.es>
2020-10-08 12:44:37 +0200

    Interfaces del jugador

--- Torneo Ajedrez/src/Vista/asignarResponsableInfantil.java
+++ Torneo Ajedrez/src/Vista/asignarResponsableInfantil.java
@@ -9,12 +9,12 @@ package Vista;
  *
  * @author Ayoub El Moussaoui
  */
-public class buscarEquipo extends javax.swing.JFrame {
+public class asignarResponsableInfantil extends javax.swing.JFrame {
 
     /**
-     * Creates new form buscarEquipo
+     * Creates new form asignarResponsableInfantil
      */
-    public buscarEquipo() {
+    public asignarResponsableInfantil() {
         initComponents();
     }
 
@@ -28,25 +28,22 @@ public class buscarEquipo extends javax.swing.JFrame {
     private void initComponents() {
 
         jScrollPane1 = new javax.swing.JScrollPane();
-        listaBuscarEquipo = new javax.swing.JList<>();
-        atras = new javax.swing.JButton();
-        apuntarse = new javax.swing.JButton();
-        jLabel1 = new javax.swing.JLabel();
+        jList1 = new javax.swing.JList<>();
+        asignar = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        listaBuscarEquipo.setModel(new javax.swing.AbstractListModel<String>() {
-            String[] strings = { "equipo 1", "equipo 2", "equipo 3" };
+        jList1.setModel(new javax.swing.AbstractListModel<String>() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
             public int getSize() { return strings.length; }
             public String getElementAt(int i) { return strings[i]; }
         });
-        jScrollPane1.setViewportView(listaBuscarEquipo);
+        jScrollPane1.setViewportView(jList1);
 
-        atras.setText("Atras");
+        asignar.setText("Asignar");
 
-        apuntarse.setText("Apuntarse");
-
-        jLabel1.setText("Buscar equipo");
+        jButton1.setText("Atras");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -57,26 +54,20 @@ public class buscarEquipo extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jScrollPane1)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(apuntarse)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(atras)))
+                        .addComponent(asignar)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 252, Short.MAX_VALUE)
+                        .addComponent(jButton1)))
                 .addContainerGap())
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(165, Short.MAX_VALUE)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 104, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(150, 150, 150))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(15, 15, 15)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 21, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 255, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(50, 50, 50)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 204, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(atras)
-                    .addComponent(apuntarse))
+                    .addComponent(asignar)
+                    .addComponent(jButton1))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
@@ -100,29 +91,28 @@ public class buscarEquipo extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(buscarEquipo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(asignarResponsableInfantil.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(buscarEquipo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(asignarResponsableInfantil.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(buscarEquipo.class.getName()).log(javaapuntarseogging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(asignarResponsableInfantil.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(buscarEquipo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(asignarResponsableInfantil.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new buscarEquipo().setVisible(true);
+                new asignarResponsableInfantil().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton apuntarse;
-    private javax.swing.JButton atras;
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JButton asignar;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JList<String> jList1;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JList<String> listaBuscarEquipo;
     // End of variables declaration//GEN-END:variables
 }
